17bf92ef08edf2344af0f9a0c1521ee7eac8a02c
Udit Sen <udisen97@gmail.com>
2023-05-16 21:12:11 +0530

    setting up default events in vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,47 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import moment from 'moment';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    events: [
+      {
+        name: 'Random Event fdsjfdsnjfnds dsfjndsjk',
+        type: 'STANDARD', // ALL/STANDARD
+        startTime: moment().subtract(3, 'hours'),
+        endTime: moment().subtract(120, 'minutes'),
+      },
+      {
+        name: 'Random Event fjndjgnfdgjdfsnjkf',
+        type: 'STANDARD', // ALL/STANDARD
+        startTime: moment().add(1, 'day').subtract(3, 'hours'),
+        endTime: moment().add(1, 'day').subtract(2, 'hours'),
+      },
+      {
+        name: 'Random Event 3 dfdsknafaksdn',
+        type: 'ALL', // ALL/STANDARD
+        startTime: moment().subtract(1, 'day').startOf('day'),
+        endTime: moment().subtract(1, 'day').endOf('day'),
+      },
+      {
+        name: 'Random Event 4 dfdsknafaksdn',
+        type: 'STANDARD', // ALL/STANDARD
+        startTime: moment().subtract(1, 'day').add(8, 'hours'),
+        endTime: moment().subtract(1, 'day').endOf('day'),
+      },
+    ],
   },
   mutations: {
+    addEvent({ events }, evt) {
+      events.push(evt);
+    },
   },
   actions: {
+    addEvent({ commit }, evt) {
+      commit('addEvent', evt);
+    },
   },
   modules: {
   },
