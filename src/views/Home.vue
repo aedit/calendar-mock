1aa7483bbf7bba161d7dab5968de1e84b74b11d5
Udit Sen <udisen97@gmail.com>
2023-05-16 20:40:34 +0530

    header with prev, next and calendar view selection

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,79 @@
 <template>
   <div class="home">
-
+    <header class="header">
+      <div class="header__navigation">
+        <button>&lt;</button>
+        <button>&gt;</button>
+      </div>
+      <div class="header__dateoptions">
+        <button>Today</button>
+        <select v-model="viewType">
+          <option value="days">Day</option>
+          <option value="weeks">Week</option>
+          <option value="months">Month</option>
+        </select>
+      </div>
+      <div class="header__create-event">
+        <button>Create Event</button>
+      </div>
+    </header>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
-  components: {
-
+  data() {
+    return {
+      viewType: 'months',
+    };
   },
+  components: {},
 };
 </script>
+
+<style lang="scss">
+.header{
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem 2rem;
+  button{
+      border: 1px solid black;
+      border-radius: 10px;
+      padding: 0.2rem 0.5rem;
+      background: white;
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+  &__navigation{
+    display: flex;
+    gap: 0.5rem;
+    button{
+      border: 1px solid black;
+      border-radius: 10px;
+      padding: 0.2rem 0.5rem;
+      background: white;
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+
+  &__dateoptions{
+    display: flex;
+    gap: 1rem
+  }
+
+  &__create-event{
+    button{
+      border-color: #45a8e4;
+      background: #45a8e4;
+      color: white;
+    }
+  }
+}
+</style>
